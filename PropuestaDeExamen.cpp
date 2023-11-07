cb28a663791edbb5bf2cd5a9f534dae5689e2735
jsepucas <jsepucas@myuax.com>
2023-11-07 11:46:48 +0100

    11

--- PropuestaDeExamen.cpp
+++ PropuestaDeExamen.cpp
@@ -8,6 +8,7 @@ int main() {
     // Repito varias veces el bucle, para "a"
     for(int a = 1; a <= limite_maximo; ++a) {
         for(int b = a ; b <= limite_maximo; ++b) {
+            //Calculo el lado "c"
             int c = std::sqrt(a * a + b * b);
 
             if(c <= limite_maximo && a + b + c <= 2 * limite_maximo && a * a + b * b == c * c) {
